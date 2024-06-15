f52a8013130825b6bc76346ec9139549b42f274a
wwrena <teikukuheia@gmail.com>
2024-06-15 17:11:13 +0300

    fix(api): added token validation so route is not being treated as static

--- app/api/v1/products/route.ts
+++ app/api/v1/products/route.ts
@@ -1,8 +1,10 @@
 import { shop_service } from "@/app/api/(services)/shop.service";
+import { faker } from "@faker-js/faker";
+import { validateAuthToken } from "../../(middleware)/jwt";
 
 export async function GET(request: Request) {
+  const user = validateAuthToken();
   const products = await shop_service.api.get_all_products();
+
   return Response.json(products);
 }
-
-export const runtime = "nodejs";
